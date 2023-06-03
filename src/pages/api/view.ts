bf48bebd3a43b6c45664a0fbf439582243aa273c
Herman Nygaard <hnygaard@microsoft.com>
2023-06-03 13:40:00 +0200

    with count

--- src/pages/api/view.ts
+++ src/pages/api/view.ts
@@ -1,8 +1,12 @@
 import { kv } from "@vercel/kv";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const slug = req.body.slug;
-  kv.incr(slug);
-  return res.status(200).json("OK");
+  if (req.method !== "POST" || !slug) return res.status(400);
+  const count = await kv.incr(slug);
+  return res.status(200).json({ count });
 }
